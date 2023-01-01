b1b18e8df06552991c6f3e1fac435654b1027aaa
akhilts03 <akhiltskumar@gmail.com>
2023-01-02 03:35:55 +0530

    changed2

--- question3/js/bootstrap.bundle.js
+++ question3/js/bootstrap.bundle.js
@@ -3,6 +3,8 @@
   * Copyright 2011-2018 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
+
+//2/3 _3v3rywh3r3_
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
